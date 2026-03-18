c6e9cdd383ef8de02482ce9dca9c27b32bd38ca8
Thomas Günther <thomas@pygimli.org>
2026-03-18 18:15:20 +0100

    ADD TEM class and sTEM functions

---
+++ stem.py
@@ -0,0 +1,196 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import empymod
+import pygimli as pg
+
+def readGEXFile(fname="sTEM.gex"):
+    """Read GEX file (a rather general function)."""
+    with open(fname) as fid:
+        lines = fid.readlines()
+
+    out = {}
+    putin = out
+    for line in lines:
+        line = line.replace("\n", "").replace("\r", "")
+        if len(line) > 2 and line[0] == "[" and line[-1] == "]":
+            sect = line[1:-1]
+            if sect != "General":
+                putin = out[sect] = {}
+
+        if "=" in line:
+            nam, val = line.split("=")
+            try:
+                putin[nam] = np.fromstring(val, dtype=float, sep=" ")
+            except ValueError:
+                putin[nam] = val
+
+    return out
+
+def collectNumData(dic, name, start=1, stop=100, num=0):
+    """Collect data from dictionary stored in numerically named keys."""
+    if num == 0:
+        for num in range(1, 4):
+            if f"{name}{start:0{num}d}" in dic:
+                break
+    i = start
+    col = []
+    for i in range(start, stop):
+        key = f"{name}{i:0{num}d}"
+        if key in dic:
+            col.append(dic[key])
+        else:
+            break
+
+    return np.array(col)
+
+
+def readSettings(filename="sTEM.gex"):
+    """Read settings."""
+    out = readGEXFile(filename)
+    cfg = {}
+    cfg["rxpos"] = out["RxCoilPosition1"]
+    cfg["txpos"] = out["TxCoilPosition1"]
+    cfg["txarea"] = out["TxLoopArea"]
+    txp = collectNumData(out, "TxLoopPoint")
+    cfg["tx"], cfg["ty"] = txp[:, 0], txp[:, 1]
+    bla = collectNumData(out, "WaveformLMPoint", num=2)
+    cfg["tL"], cfg["vL"] = bla[:, 0], bla[:, 1]
+    bla = collectNumData(out, "WaveformHMPoint", num=2)
+    cfg["tH"], cfg["vH"] = bla[:, 0], bla[:, 1]
+    cfg["timeL"] = collectNumData(out, "GateTimeLM")[:, 0]
+    cfg["timeH"] = collectNumData(out, "GateTimeHM")[:, 0]
+    return cfg
+
+def readSettings1(filename="sTEM.gex"):
+    with open(filename) as fid:
+        lines = fid.readlines()
+
+    cfg = {}
+    cfg["tL"], cfg["vL"] = np.genfromtxt(lines[20:60], usecols=[1, 2], unpack=True)
+    cfg["tH"], cfg["vH"] = np.genfromtxt(lines[61:109], usecols=[1, 2], unpack=True)
+    cfg["timeL"] = np.genfromtxt(lines[111:117], usecols=[1])
+    cfg["timeH"] = np.genfromtxt(lines[119:141], usecols=[1])
+    cfg["tx"], cfg["ty"] = np.genfromtxt(lines[15:19], usecols=[1, 2], unpack=True)
+    cfg["rxpos"] = np.genfromtxt(lines[7:8], usecols=[1, 2, 3])
+    cfg["txarea"] = np.genfromtxt(lines[14:15], usecols=[1])
+    return cfg
+
+
+def bandpass(inp, p_dict):
+    """Butterworth-type filter (implemented from simpegEM1D.Waveforms.py)."""
+    cutofffreq = 1e8  # Determined empirically for TEM-FAST
+    h = (1 + 1j*p_dict["freq"]/cutofffreq)**-1
+    h *= (1 + 1j*p_dict["freq"]/3e5)**-1
+    p_dict["EM"] *= h[:, None]
+
+
+class sTEMBlockModelling(pg.frameworks.Block1DModelling):
+
+    def __init__(self, **kwargs):
+        cfg = kwargs.pop("cfg", {})
+        super().__init__(**kwargs)
+        if isinstance(cfg, str):
+            cfg = readSettings(cfg)
+
+        self.signalL = {'nodes': cfg["tL"], 'amplitudes': cfg["vL"], 'signal': 1}
+        self.signalH = {'nodes': cfg["tH"], 'amplitudes': cfg["vH"], 'signal': 1}
+        self.timeL = cfg["timeL"]
+        self.timeH = cfg["timeH"]
+
+        self.kw = dict(
+            src=[[cfg["tx"][-1], *cfg["tx"]], # x1
+            [*cfg["tx"], cfg["tx"][0]], # x2
+            [cfg["ty"][-1], *cfg["ty"]], # y1
+            [*cfg["ty"], cfg["ty"][0]], # y2
+            0, 0],
+            strength=1/cfg["txarea"],
+            verb=0,
+            rec = np.concatenate([cfg["rxpos"], [0, 90]]),       # Receiver at the origin, vertical.
+            mrec="b",                   # Receiver: dB/dt
+            srcpts=3,                   # Approx. the finite dip. with 3 points.
+            ftarg={"dlf": "key_81_2009"},  # Shorter, faster filters.
+            htarg={"dlf": "key_101_2009", "pts_per_dec": -1},
+            bandpass={"func": bandpass}
+            )
+
+    @property
+    def t(self):
+        return np.concatenate([self.timeL, self.timeH])
+
+    def response(self, model):
+        """Return model response."""
+        thk = model[:self.nLayers-1]
+        res = model[self.nLayers-1:]
+        outL = empymod.model.bipole(
+            depth=np.concatenate([[0], np.cumsum(np.atleast_1d(thk))]), # Depth-model.
+            res=np.concatenate([[2e14], np.atleast_1d(res)]),      # Resistivity model.
+            signal=self.signalL,
+            freqtime=self.timeL,      # Wanted times.
+            **self.kw)
+        outH = empymod.model.bipole(
+            depth=np.concatenate([[0], np.cumsum(np.atleast_1d(thk))]), # Depth-model.
+            res=np.concatenate([[2e14], np.atleast_1d(res)]),      # Resistivity model.
+            signal=self.signalH,
+            freqtime=self.timeH,      # Wanted times.
+            **self.kw)
+        return np.concatenate([outL.sum(axis=1), outH.sum(axis=1)])
+
+
+class sTEMRhoModelling(pg.frameworks.MeshModelling):
+
+    def __init__(self, thk, **kwargs):
+        self.thk = thk
+        cfg = kwargs.pop("cfg", "sTEM.gex")
+        self.mesh_ = pg.meshtools.createMesh1D(len(thk)+1)
+        super().__init__(mesh=self.mesh_)
+        if isinstance(cfg, str):
+            cfg = readSettings(cfg)
+
+        self.signalL = {'nodes': cfg["tL"], 'amplitudes': cfg["vL"], 'signal': 1}
+        self.signalH = {'nodes': cfg["tH"], 'amplitudes': cfg["vH"], 'signal': 1}
+        self.timeL = cfg["timeL"]
+        self.timeH = cfg["timeH"]
+
+        self.kw = dict(
+            src=[[cfg["tx"][-1], *cfg["tx"]], # x1
+            [*cfg["tx"], cfg["tx"][0]], # x2
+            [cfg["ty"][-1], *cfg["ty"]], # y1
+            [*cfg["ty"], cfg["ty"][0]], # y2
+            0, 0],
+            strength=1/cfg["txarea"],
+            verb=0,
+            depth = np.concatenate([[0], np.cumsum(np.atleast_1d(thk))]),
+            rec = np.concatenate([cfg["rxpos"], [0, 90]]), # Receiver at the origin, vertical.
+            mrec="b",                   # Receiver: dB/dt
+            srcpts=3,                   # Approx. the finite dip. with 3 points.
+            ftarg={"dlf": "key_81_2009"},  # Shorter, faster filters.
+            htarg={"dlf": "key_101_2009", "pts_per_dec": -1},
+            bandpass={"func": bandpass}
+            )
+
+    @property
+    def t(self):
+        return np.concatenate([self.timeL, self.timeH])
+
+    def response(self, model):
+        """Return model response."""
+        return np.concatenate([
+            empymod.model.bipole(
+                res=np.concatenate([[2e14], model]),
+                signal=self.signalL,
+                freqtime=self.timeL,      # Wanted times.
+                **self.kw).sum(axis=1),
+            empymod.model.bipole(
+                res=np.concatenate([[2e14], model]),
+                signal=self.signalH,
+                freqtime=self.timeH,      # Wanted times.
+                **self.kw).sum(axis=1)])
+
+    def createStartVector(self, data):
+        return pg.Vector(len(self.thk)+1, 250.)
+
+# %%
+if __name__ == "__main__":
+    f = sTEMRhoModelling(thk=np.arange(2, 28, 2), cfg="sTEM.gex")
+    rho = pg.Vector(len(f.thk)+1, 100.)
+    print(f(rho))
---
+++ tem.py
@@ -0,0 +1,204 @@
+# %%
+import utm
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from stem import readSettings, sTEMRhoModelling
+# from stem import sTEMBlockModelling
+import pygimli as pg
+
+def rhoa(t, dbzdt, m=1.0):
+    mu0 = 4e-7*np.pi
+    return mu0 / np.pi * (mu0 * m / 20.0)**(2/3) * np.abs(dbzdt)**(-2/3) * t**(-5/3)
+
+# %%
+class TEM:
+    """Class for processing TEM data."""
+    def __init__(self, filename:str=None, cfg=None, **kwargs):
+        self.thk = kwargs.pop("thk", np.arange(2, 28, 2))
+        if cfg is not None:
+            if isinstance(cfg, str):
+                self.readCFG(cfg)
+            else:
+                self.cfg = cfg
+        if filename is not None:
+            self.readData(filename)
+
+    def __str__(self):
+        """String representation."""
+        return f"TEM profile with {len(self.DATA):d} soundings\n" + \
+            f"{len(self.t):d} times ({min(self.t)}-{max(self.t)})"
+
+    def readCFG(self, cfg):
+        """Read data file (xyz)."""
+        self.cfg = readSettings(cfg)
+        self.f = sTEMRhoModelling(thk=self.thk, cfg=self.cfg)
+        self.t = self.f.t
+
+    def showWaveform(self):
+        """Show current waveform."""
+        fig, ax = plt.subplots()
+        ax.plot(self.cfg["tL"], self.cfg["vL"], label="LM")
+        ax.plot(self.cfg["tH"], self.cfg["vH"], label="HM")
+        ax.set_xlabel("t [s]")
+        ax.legend()
+        ax.grid()
+        return ax
+
+    def readData(self, filename):
+        """Read data file (xyz)."""
+        self.data = pd.read_csv(filename, skiprows=2, sep=r"\s+")
+        self.extractData()
+
+    def extractData(self):
+        """Extract data from dataframe into arrays."""
+        nL = len(self.cfg["timeL"])
+        CH1 = np.column_stack([self.data[f"dbdt{i:03d}_Ch1"] for i in range(1, nL+1)])
+        SD1 = np.column_stack([self.data[f"stdF{i:03d}_Ch1"] for i in range(1, nL+1)])
+        nH = len(self.cfg["timeH"])
+        CH2 = np.column_stack([self.data[f"dbdt{i:03d}_Ch2"] for i in range(1, nH+1)])
+        SD2 = np.column_stack([self.data[f"stdF{i:03d}_Ch2"] for i in range(1, nH+1)])
+        print(CH1.shape, CH2.shape)
+        self.DATA = np.hstack([CH1, CH2])
+        self.SD = np.hstack([SD1, SD2])
+        self.calcRhoa()
+
+    def calcRhoa(self, rmin=1, rmax=10000):
+        """Compute apparent resistivity."""
+        self.RHOA = np.zeros_like(self.DATA)
+        for n, data in enumerate(self.DATA):
+            self.RHOA[n] = rhoa(self.t, data)
+
+        self.RHOA[self.RHOA < rmin] = np.nan
+        self.RHOA[self.RHOA > rmax] = np.nan
+
+    def showRhoa(self, rmin=10, rmax=500, **kwargs):
+        """Show apparent resistivity."""
+        kwargs.setdefault("cmap", "Spectral_r")
+        plt.imshow(np.log10(self.RHOA.T),
+                   vmin=np.log10(rmin), vmax=np.log10(rmax),
+                   **kwargs)
+
+    def showSounding(self, n=0, **kwargs):
+        """Show single sounding."""
+        kwargs.setdefault("marker", "+")
+        kwargs.setdefault("ls", ":")
+        fig, ax = plt.subplots()
+        err = self.SD[n]*np.abs(self.DATA[n])
+        ax.errorbar(self.t, self.DATA[n], yerr=err, **kwargs)
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.grid()
+
+    def showPositions(self):
+        """Show sounding positions."""
+        fig, ax = plt.subplots()
+        x, y, *_ = utm.from_latlon(self.data["Latitude"].to_numpy(),
+                                   self.data["Longitude"].to_numpy())
+        ax.plot(x, y, "x")
+        for i in range(0, len(x), 10):
+            ax.text(x[i], y[i], str(i), va="center", ha="center")
+
+        ax.set_aspect(1.0)
+        ax.grid()
+
+    def filter(self, tmin=0, tmax=1e9, nmin=0, nmax=9999, n=None):
+        """Filter data."""
+        doex = False
+        if nmin > 0:
+            self.data.drop(np.nonzero(self.data.index < nmin)[0], inplace=True)
+            doex = True
+        if nmax < self.DATA.shape[0]:
+            self.data.drop(np.nonzero(self.data.index > nmax)[0], inplace=True)
+            doex = True
+        if n is not None:
+            self.data.drop(n, inplace=True)
+            doex = True
+        if doex:
+            self.extractData()
+
+    def invertSounding(self, n=0, show=True):
+        """Invert data."""
+        inv = pg.Inversion(fop=self.f)
+        # inv.dataTrans = "log" # not really necessary
+        data = np.abs(self.DATA[n])
+        err = np.maximum(self.SD[n], 0.015)
+        err[self.DATA[n] <1e-15] = 1e8
+        err[err > 1] = 1e8
+        self.res1d = inv.run(data, err, verbose=1, startModel=self.f.createStartVector(data))
+        self.inv1d = inv
+        if show:
+            fig, ax = plt.subplots(ncols=2)
+            pg.viewer.mpl.drawModel1D(ax[0], self.thk, self.res1d, plot="semilogx")
+            ax[0].invert_yaxis()
+            ax[1].errorbar(self.t, data, yerr=err*data, marker="+", ls=":", label="data")
+            ax[1].loglog(self.t, inv.response, '-', label="model response B")
+            ax[1].legend()
+            # ax[1].set_ylim(1e-14, 1e-7)
+            ax[1].grid()
+
+    def invertAll(self, show=True):
+        """Invert all data individually."""
+        self.invertSounding(0, show=False)
+        res = self.res1d
+        self.MODELS = [res]
+        self.CHI2 = [self.inv1d.chi2()]
+        for n in range(1, len(self.DATA)):
+            data = np.abs(self.DATA[n])
+            err = np.maximum(self.SD[n], 0.01)
+            err[self.DATA[n] <1e-15] = 1e8
+            err[err > 1] = 1e8
+            res = self.inv1d.run(data, err, verbose=0, startModel=res)
+            self.MODELS.append(res)
+            self.CHI2.append(self.inv1d.chi2())
+            pg.info(f"{n}: chi2={self.CHI2[-1]:.2f} ({self.inv1d.iter:d} iter)")
+
+        if show:
+            self.showResults()
+
+    def invertLCI(self, **kwargs):
+        """Invert all data with lateral constraints."""
+        pass
+
+    def invertSCI(self, **kwargs):
+        """Invert all data with lateral constraints."""
+        pass
+
+    def showResults(self, usepos=True, **kwargs):
+        kwargs.setdefault("cMin", 10)
+        kwargs.setdefault("cMin", 500)
+        kwargs.setdefault("cMap", "Spectral_r")
+        if usepos is not None:
+            x, y, *_ = utm.from_latlon(self.data["Latitude"].to_numpy(),
+                                    self.data["Longitude"].to_numpy())
+            if usepos == "x": # Easting
+                kwargs["x"] = x
+            elif usepos == "y": # Northing
+                kwargs["x"] = y
+            else: # tape measure
+                dx = np.sqrt(np.diff(x)**2 +np.diff(y)**2)
+                kwargs["x"] = np.hstack([0, np.cumsum(dx)])
+        if "Elevation" in self.data:
+            kwargs.setdefault("topo", self.data["Elevation"].to_numpy())
+        return pg.viewer.mpl.showStitchedModels(
+            self.MODELS, thk=self.thk, **kwargs)
+
+# %%
+if __name__ == "__main__":
+    self = TEM("data/Madsen2026/sTEM2.xyz",
+               cfg="data/Madsen2026/sTEM.gex")
+    print(self)
+    # %%
+    ax = self.showWaveform()
+    ax.set_xlim(-1e-7, 5e-6)
+    ax.set_ylim(-0.001, 1.001)
+    # %%
+    self.filter(nmax=20)
+    self.filter(n=[9, 10])
+    # self.showPositions()
+    # %%
+    # self.showRhoa(rmax=100)
+    # self.invertSounding()
+    # self.invertAll()
+    # %%
+    # f = pg.frameworks.MultiFrameModelling(self.f)
